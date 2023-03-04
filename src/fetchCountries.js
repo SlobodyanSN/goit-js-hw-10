a130c1f4cefed3b50df660d440befe22c7bf66cf
SlobodyanSN <110773219+SlobodyanSN@users.noreply.github.com>
2023-03-04 18:02:16 +0200

    prepearing

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+function fetchCountries(name) {
+    const BASE_URL = `https://restcountries.com/v3.1/name/`
+   return fetch(`${BASE_URL}{name}`)
+
+
+    .then(resp => {if (!resp.ok) {
+        throw new Error(resp.statusText)
+    };
+    console.log(resp);
+    return resp.json()
+})
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './css/styles.css';
 
+// import { useDebounce } from "use-debounce";
+
+var debounce = require('lodash.debounce');
+
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector(`#search-box`);
+input.addEventListener(`input`, onInput);
+
+function onInput (e) {
+    console.log(e.eventTarget.value);
+}
